21b6431def100cdf9c9b80841618324ad4892a0e
Hritick lohani <101631134+HL7988@users.noreply.github.com>
2023-03-30 00:43:23 +0530

    app.py created initially

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import altair as alt
+st.title("Welcome to my profile ") 
+
+#st.header("Hi I")
+#st.subheader("subheader")
+
+st.text("Hi I am Hritick Lohani and this web page is my profile")
